9f45ef067cef89d2c7ee1db376119837b1a89a44
Caleb Gasser <1561693+calebgasser@users.noreply.github.com>
2023-10-08 20:51:01 -0500

    still not working

--- turso/default.nix
+++ turso/default.nix
@@ -1,5 +1,8 @@
 final: prev: {
-  turso-cli = prev.turso-cli.overrideAttrs (_: rec {
+  turso-cli = prev.buildGo121Module rec {
+    inherit (prev.turso-cli.drvAttrs)
+      pname nativeBuildInputs tags preBuild preCheck postInstall;
+    inherit (prev.turso-cli) meta;
     version = "0.86.0";
     src = prev.fetchFromGitHub {
       owner = "tursodatabase";
@@ -7,6 +10,16 @@ final: prev: {
       rev = "v${version}";
       hash = "sha256-HSRlIRhYDXnQNBTc9gCT6yQizvzQcIW0pwlvh1Spf0U=";
     };
-    vendorHash = null;
-  });
+    vendorSha256 = "sha256-6ZvqVVSQVReFef/Ku2G9+0uqAZb1GhqGSlr0rZbULbA=";
+  };
+  # turso-cli = prev.turso-cli.overrideAttrs (_: rec {
+  #   version = "0.86.0";
+  #   src = prev.fetchFromGitHub {
+  #     owner = "tursodatabase";
+  #     repo = "turso-cli";
+  #     rev = "v${version}";
+  #     hash = "sha256-HSRlIRhYDXnQNBTc9gCT6yQizvzQcIW0pwlvh1Spf0U=";
+  #   };
+  #   vendorHash = null;
+  # });
 }
